cce9971d4a0b6c3439ab3cfb58d80c3a73cb255e
gskalfas <kalfas@iacs.gr>
2024-03-08 12:03:01 +0200

    update the computations of dt/h

--- test_tasty.sql
+++ test_tasty.sql
@@ -7,13 +7,17 @@ GO
 
 --QUERING THE TEMPERATURE DIFFERENCES (DT)
 
-SELECT EndDateTime, TagName, Average ,OPCQuality FROM AnalogSummaryHistory
-WHERE TAGNAME in ('Mixer100_Temperature_PV','Mixer200_Temperature_PV','Mixer300_Temperature_PV','Mixer400_Temperature_PV')
-		AND StartDateTime >= '2024-3-6 10:20:00'
-		AND EndDateTime <= '2024-3-6 10:30:00'
-        AND wwCycleCount = 1
-        AND wwVersion = 'LATEST'
+DECLARE @DATE NVARCHAR(100)
+SET @DATE = '2024-03-06 10:30:00.000'
 
+SELECT CONVERT(DATETIME, @DATE) AS EndDateTime, TagName, AVG(Value) AS MEAN_DTH FROM AnalogHistory
+WHERE TAGNAME IN ('Mixer100_Temperature_PV','Mixer200_Temperature_PV','Mixer300_Temperature_PV','Mixer400_Temperature_PV')
+		AND DateTime >= '2024-3-6 10:20:00'
+		AND DateTime <= '2024-3-6 10:30:00'
+        --AND wwCycleCount = 1
+        AND wwVersion = 'LATEST'
+        AND wwRetrievalMode = 'Slope'
+GROUP BY TagName
 
 --QUERING THE KG PRODUCTION 
 
@@ -26,4 +30,31 @@ WHERE TAGNAME LIKE 'Mixer100_Level_PV'
     AND wwCycleCount = 1
     AND wwVersion = 'LATEST'
 
-SELECT @kg1 AS PRODUCTION_KG
+SELECT EndDateTime, TagName, @kg1 AS PRODUCTION_KG, OPCQuality FROM AnalogSummaryHistory
+WHERE TAGNAME LIKE 'Mixer100_Level_PV'
+    AND StartDateTime >= '2024-3-6 10:20:00'
+	AND EndDateTime <= '2024-3-6 10:30:00'
+    AND wwCycleCount = 1
+    AND wwVersion = 'LATEST'
+
+
+--QUERING THE INTENSITY KPI
+
+/*CREATE TABLE #ENERGIES (EndDateTime DATETIME, TagName NVARCHAR(100), ENERGY FLOAT, OPCQuality INT)
+
+INSERT INTO #ENERGIES (EndDateTime , TagName , ENERGY , OPCQuality)
+*/
+SELECT EndDateTime, TagName, Last - First AS ENERGY, OPCQuality FROM AnalogSummaryHistory
+WHERE TAGNAME IN ('Mixer100_Temperature_PV','Mixer200_Temperature_PV','Mixer300_Temperature_PV','Mixer400_Temperature_PV')
+    AND StartDateTime >= '2024-3-6 10:20:00'
+	AND EndDateTime <= '2024-3-6 10:30:00'
+    AND wwCycleCount = 1
+    AND wwVersion = 'LATEST'
+
+
+
+DECLARE @INTENSITY_TAGNAME NVARCHAR(100)
+SET @INTENSITY_TAGNAME = 'POTATO_LINE1_INTESTITY'
+
+SELECT CONVERT(DATETIME, @DATE) AS EndDateTime, @INTENSITY_TAGNAME AS TagName, SUM(ENERGY)/@kg1 AS INTENSITY_KPI FROM #ENERGIES
+--DROP TABLE #ENERGIES
